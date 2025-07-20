efc79a8dcf97623b69540a5afb692fa1817704c1
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-07-20 20:29:36 +0200

    improved typing support in type aliases

--- bindings/python/sparsely/__init__.py
+++ bindings/python/sparsely/__init__.py
@@ -1,11 +1,13 @@
+from typing_extensions import TypeAlias
+
 from .lil import LIL as LIL
 from .csr import CSR as CSR
 from .csc import CSC as CSC
 from .cholesky import cholesky, solve_cholesky
 
-csr_array = CSR
-csc_array = CSC
-lil_array = LIL
+csr_array: TypeAlias = CSR
+csc_array: TypeAlias = CSC
+lil_array: TypeAlias = LIL
 
 __all__ = [
     "CSR", "csr_array",
