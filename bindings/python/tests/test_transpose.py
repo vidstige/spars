693e54551fdcbf624113537c5613a2198293e1d6
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-07-21 21:39:27 +0200

    refactor: smarter transpose tests

--- bindings/python/tests/test_transpose.py
+++ bindings/python/tests/test_transpose.py
@@ -1,26 +1,31 @@
-from typing import Tuple
 import numpy as np
-from sparsely import CSR
-
-
-def matrix() -> Tuple[np.ndarray, CSR]:
-    A_dense = np.array([
-        [1.0, 0.0, 2.0],
-        [0.0, 3.0, 0.0],
-        [4.0, 0.0, 5.0]
-    ])
-    return A_dense, CSR.from_dense(A_dense)
+from sparsely import CSR, CSC
+from .matrices import easy3x3
 
 
 def test_csr_transpose_roundtrip():
-    A_dense, A = matrix()
+    A_dense = easy3x3()
+    A = CSR.from_dense(A_dense)
     A_T = A.T
     A_back = A_T.T
-
     np.testing.assert_allclose(A_back.todense(), A_dense)
 
 
-def test_dense():
-    A_dense, A = matrix()
-    AT = A.T
-    np.testing.assert_allclose(AT.todense(), A_dense.T)
+def test_csc_transpose_roundtrip():
+    A_dense = easy3x3()
+    A = CSC.from_dense(A_dense)
+    A_T = A.T
+    A_back = A_T.T
+    np.testing.assert_allclose(A_back.todense(), A_dense)
+
+
+def test_csr_transpose_type():
+    A = CSR.from_dense(easy3x3())
+    A_T = A.T
+    assert isinstance(A_T, CSC), "Transpose of CSR should be CSC"
+
+
+def test_csc_transpose_type():
+    A = CSC.from_dense(easy3x3())
+    A_T = A.T
+    assert isinstance(A_T, CSR), "Transpose of CSC should be CSR"
