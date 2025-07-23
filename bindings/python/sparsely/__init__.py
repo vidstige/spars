cdd7f25dcf1171c8286ba4f920034d71393430ae
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-07-23 13:02:48 +0200

    rm typing extensions

--- bindings/python/sparsely/__init__.py
+++ bindings/python/sparsely/__init__.py
@@ -1,13 +1,11 @@
-from typing_extensions import TypeAlias
-
 from .lil import LIL as LIL
 from .csr import CSR as CSR
 from .csc import CSC as CSC
 from .cholesky import cholesky, solve_cholesky
 
-csr_array: TypeAlias = CSR
-csc_array: TypeAlias = CSC
-lil_array: TypeAlias = LIL
+csr_array = CSR
+csc_array = CSC
+lil_array = LIL
 
 __all__ = [
     "CSR", "csr_array",
