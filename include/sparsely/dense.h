cd4ff764ef5de453ab38f7141d594adde0a75fc3
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-07-27 09:09:12 +0200

    more dense operations

--- include/sparsely/dense.h
+++ include/sparsely/dense.h
@@ -8,4 +8,11 @@ typedef struct {
 dense_t dense_empty(int n); // uninitiaized values
 dense_t dense_zeros(int n); // zero initialized values
 dense_t dense_copy(int n, const double *data); // copies values
+void dense_copy_to(dense_t *dst, const dense_t *src);
+dense_t *dense_clone(const dense_t *src);
 void dense_destroy(const dense_t *v);
+
+double dense_dot(const dense_t *a, const dense_t *b);
+double dense_norm(const dense_t *v);
+void dense_scale(dense_t *v, double alpha);
+void dense_add_scaled(dense_t *y, double alpha, const dense_t *x);  // y ← y + αx
--- src/dense.c
+++ src/dense.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "sparsely/dense.h"
 
 dense_t dense_empty(int n) {
@@ -37,7 +38,45 @@ dense_t dense_copy(int n, const double *data) {
     }
     return v;
 }
+void dense_copy_to(dense_t *dst, const dense_t *src) {
+    assert(dst->n == src->n);
+    for (int i = 0; i < dst->n; ++i)
+        dst->values[i] = src->values[i];
+}
+
+dense_t *dense_clone(const dense_t *src) {
+    dense_t *copy = malloc(sizeof(dense_t));
+    copy->n = src->n;
+    copy->values = malloc(sizeof(double) * src->n);
+    for (int i = 0; i < src->n; ++i)
+        copy->values[i] = src->values[i];
+    return copy;
+}
 
 void dense_destroy(const dense_t *v) {
     if (v) free(v->values);
+}
+
+// operations
+double dense_dot(const dense_t *a, const dense_t *b) {
+    assert(a->n == b->n);
+    double sum = 0.0;
+    for (int i = 0; i < a->n; ++i)
+        sum += a->values[i] * b->values[i];
+    return sum;
+}
+
+double dense_norm(const dense_t *v) {
+    return sqrt(dense_dot(v, v));
+}
+
+void dense_scale(dense_t *v, double alpha) {
+    for (int i = 0; i < v->n; ++i)
+        v->values[i] *= alpha;
+}
+
+void dense_add_scaled(dense_t *y, double alpha, const dense_t *x) {
+    assert(y->n == x->n);
+    for (int i = 0; i < y->n; ++i)
+        y->values[i] += alpha * x->values[i];
 }
