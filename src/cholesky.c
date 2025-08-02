34fa410a94c77eb390669f2620e7aab2a55cb42e
Samuel Carlsson <samuel.carlsson@gmail.com>
2025-08-02 21:02:58 +0200

    cholesky no pointer aliasing

--- src/cholesky.c
+++ src/cholesky.c
@@ -5,16 +5,23 @@
 #include "sparsely/alloc.h"
 #include "sparsely/csc.h"
 
-csc_t *cholesky_factor(const csc_t *A) {
-    int n = A->ncols;
-    if (A->nrows != n) {
+static inline csc_t *cholesky(
+    int nrows, int ncols, int nnz,
+    const int *restrict a_colptr,
+    const int *restrict a_rowind,
+    const double *restrict a_pointer_values)
+{
+    const double *a_values = SPARSELY_ASSUME_ALIGNED(a_pointer_values);
+
+    int n = ncols;
+    if (nrows != n) {
         fprintf(stderr, "Matrix is not square.\n");
         return NULL;
     }
 
     int *colptr = calloc(n + 1, sizeof(int));
-    int *rowind = malloc(A->nnz * sizeof(int));  // overallocate
-    double *values = sparsely_alloc(SPARSELY_ALIGNMENT, A->nnz * sizeof(double));
+    int *rowind = malloc(nnz * sizeof(int));  // overallocate
+    double *values = sparsely_alloc(SPARSELY_ALIGNMENT, nnz * sizeof(double));
     if (!colptr || !rowind || !values) return NULL;
 
     double *work = sparsely_alloc(SPARSELY_ALIGNMENT, n * sizeof(double));
@@ -29,10 +36,10 @@ csc_t *cholesky_factor(const csc_t *A) {
         int pattern_len = 0;
 
         // Step 1: copy A[j:n, j] into work
-        for (int idx = A->colptr[j]; idx < A->colptr[j + 1]; ++idx) {
-            int i = A->rowind[idx];
+        for (int idx = a_colptr[j]; idx < a_colptr[j + 1]; ++idx) {
+            int i = a_rowind[idx];
             if (i >= j) {
-                work[i] = A->values[idx];
+                work[i] = a_values[idx];
                 pattern[pattern_len++] = i;
             }
         }
@@ -93,11 +100,16 @@ csc_t *cholesky_factor(const csc_t *A) {
     L->nnz = nz;
     L->colptr = colptr;
     L->rowind = realloc(rowind, nz * sizeof(int));
-    L->values = realloc(values, nz * sizeof(double));
     L->values = sparsely_realloc(
-        L->values, A->nnz * sizeof(double),
+        values, nnz * sizeof(double),
         nz * sizeof(double),
         SPARSELY_ALIGNMENT
     );
     return L;
+}
+
+csc_t *cholesky_factor(const csc_t *A) {
+    return cholesky(
+        A->nrows, A->ncols, A->nnz,
+        A->colptr, A->rowind, A->values);
 }
